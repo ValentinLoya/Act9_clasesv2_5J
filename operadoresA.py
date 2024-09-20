218a5442ed023025cea46b52be2655554a9ca9c5
Valentin Loya <a.22308051281244@cbtis128.edu.mx>
2024-09-20 09:39:30 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,44 @@
+print("Actividad 9 clases version 3")
+print("Valentin Loya Mat:22308051281244")
+# zona de clases
+class Operadores1244:
+    # zona de funciones
+    def suma1244(self,V,L):
+        s1244=V+L
+        print(f"La suma de {V} + {L} = {s1244}")
+    def resta1244(self,A,O):
+        r1244=A-O
+        print(f"La resta de {A} - {O} = {r1244}")
+    def mult1244(self,D,U):
+        m1244=D*U
+        print(f"La multiplicacion de {D} * {U} = {m1244}")
+    def div1244(self,E,I):
+        d1244=E/I
+        print(f"La division de {E} / {I} = {d1244}")
+    def mod1244(self,Z,X):
+        mo1244=Z%X
+        print(f"El modulo de {Z} % {X} = {mo1244}")
+    def exp1244(self,D,G):
+        e1244=D**G
+        print(f"El exponente de {D} ** {G} = {e1244}")
+    def coc1244(self,K,P):
+        c1244=K//P
+        print(f"El cociente de {K} // {P} = {c1244}")
+# creacion del objeto
+operaLoya=Operadores1244()
+
+# zona de uso de objeto
+print(" Funcion suma")
+operaLoya.suma1244(5,4)
+print(" Funcion resta")
+operaLoya.resta1244(9,5)
+print(" Funcion multiplicacion")
+operaLoya.mult1244(2,5)
+print(" Funcion division")
+operaLoya.div1244(20,10)
+print(" Funcion modulo")
+operaLoya.mod1244(7,5)
+print(" Funcion exponente")
+operaLoya.exp1244(9,2)
+print(" Funcion Cociente")
+operaLoya.coc1244(5,4)
